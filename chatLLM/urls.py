f7a450491410e89ed07bbe52d8fb4d21e10ff6f5
Gerard Magla <gmagladev@gmail.com>
2023-07-14 19:52:22 +0000

    splitoff chatLLM FINAL

--- chatLLM/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='chat'),
-    path('process_prompt', views.process_prompt, name='process_prompt'),
-]
