40c5a625963b0b64dcb8ef87d51efe294ac6599e
Gerard Magla <gmagladev@gmail.com>
2023-08-26 20:45:35 +0000

    added "https://germagla.github.io/django-react-todo-frontend/" to allowed CORS origins, to enable frontend communication

--- projectlio0/settings.py
+++ projectlio0/settings.py
@@ -150,6 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://ec2-13-39-139-220.eu-west-3.compute.amazonaws.com",
     "http://localhost",
     "http://localhost:3000",
+    "https://germagla.github.io/django-react-todo-frontend/",
 ]
 
 CORS_ORIGIN_WHITELIST = [
